5749c4327d739153a6fdf38a280bd47f25337b03
Alberto Rodriguez <arodu.dev@gmail.com>
2020-07-06 21:32:24 -0400

    update fix, add multi-language

--- index.php
+++ index.php
@@ -2,24 +2,32 @@
   $message = [
     'es'=>'Web en construcción',
     'en'=>'Web under construction',
+    'pt'=>'Web em construção',
+    'ge'=>'Web im Aufbau',
+    'fr'=>'Web en construction',
+    'ru'=>'Сеть в разработке',
   ];
-  $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+  $lang = strtolower(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
+  $lang_default = 'en';
+  $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+  $name = $_SERVER['SERVER_NAME'];
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= $_SERVER['SERVER_NAME'] ?></title>
+  <title><?= $name ?></title>
   <link rel="stylesheet" href="style.css">
 
 </head>
 <body class="dark">
   <div class="wrap">
     <div class="message">
-      <h1><?= isset($message[$lang]) ? $message[$lang] : $message['en'] ?></h1>
-      <a href="<?= $_SERVER['HTTP_HOST'] ?>">
-        <?= $_SERVER['SERVER_NAME'] ?>
+      <h1><?= isset($message[$lang]) ? $message[$lang] : $message[$lang_default] ?></h1>
+      <a href="<?= $link ?>">
+        <?= $name ?>
       </a>
     </div>
   </div>
