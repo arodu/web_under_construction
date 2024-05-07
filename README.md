0d1950b9739b559c2c8cef03eea8317c7c25ac3a
Alberto Rodriguez <arodu.dev@gmail.com>
2024-05-07 11:21:25 -0400

    chore: Update index.php and style.css for multi-language support and improve code formatting

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # Web under construction template
 
+This is a simple template for a web page that is under construction. It provides a clean and minimalistic design with a dark and light mode option.
+
+## Usage
+
+To use this template, simply clone the repository and open the `index.html` file in your browser.
+To switch between dark and light mode, change the class of the body tag to either `dark` or `light`.
+
 ```html
 <body class="dark">
 
 <body class="light">
 ```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- index.php
+++ index.php
@@ -1,39 +1,59 @@
 <?php
-  $message = [
-    'es'=>'Web en construcción',
-    'it'=>'Web in costruzione',
-    'en'=>'Web under construction',
-    'pt'=>'Web em construção',
-    'de'=>'Web im Aufbau',
-    'fr'=>'Web en construction',
-    'ru'=>'Сеть в разработке',
-    'nl'=>'Web in aanbouw',
-    'ja'=>'建設中のウェブ',
-    'ko'=>'웹 구축 중',
-  ];
-  $lang = strtolower(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
-  $lang_default = 'en';
-  $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-  $name = $_SERVER['SERVER_NAME'];
+
+/**
+ * Web under construction
+ * 
+ * @package Web under construction
+ * @version 1.0.0
+ * @author  https://github.com/arodu
+ * @license MIT
+ */
+
+// Background image
+$background = './background.jpg';
+
+// Message in different languages
+$message = [
+    'en' => 'Web under construction',
+    'es' => 'Web en construcción',
+    'it' => 'Web in costruzione',
+    'pt' => 'Web em construção',
+    'de' => 'Web im Aufbau',
+    'fr' => 'Web en construction',
+    'ru' => 'Сеть в разработке',
+    'nl' => 'Web in aanbouw',
+    'ja' => '建設中のウェブ',
+    'ko' => '웹 구축 중'
+];
+
+// Default language
+$lang_default = 'en';
 ?>
 
+<?php
+$lang = strtolower(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
+$link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$name = $_SERVER['SERVER_NAME'];
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= $name ?></title>
-  <link rel="stylesheet" href="style.css">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $name ?></title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body class="dark">
-  <div class="wrap">
-    <div class="message">
-      <h1><?= isset($message[$lang]) ? $message[$lang] : $message[$lang_default] ?></h1>
-      <a href="<?= $link ?>">
-        <?= $name ?>
-      </a>
+    <div class="wrap" style="background-image: url(<?= $background ?>);">
+        <div class="message">
+            <h1><?= isset($message[$lang]) ? $message[$lang] : $message[$lang_default] ?></h1>
+            <a href="<?= $link ?>">
+                <?= $name ?>
+            </a>
+        </div>
     </div>
-  </div>
 </body>
+
 </html>
